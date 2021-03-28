5917f4df985e0d87876cb05b299c3928b5e3dd49
Jorge Muñoz Aguado <40457175+JorgeMunnozAguado@users.noreply.github.com>
2021-03-28 16:49:40 +0200

    Added header (which section you are)

--- scripts/style/style.js
+++ scripts/style/style.js
@@ -1,23 +1,70 @@
 
-// To Top scroll
 
-//Get the button
-var mybutton = document.getElementById("myBtn");
+// cache the navigation links 
+var $navigationLinks = $('#index > ul > a > li');
+// cache (in reversed order) the sections
+var $sections = $($(".link").get().reverse());
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// map each section id to their corresponding navigation link
+var sectionIdTonavigationLink = {};
+$sections.each(function() {
+    var id = $(this).attr('id');
+    sectionIdTonavigationLink[id] = $('#index > ul > a[href=\\#' + id + '] > li');
+});
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+// throttle function, enforces a minimum time interval
+function throttle(fn, interval) {
+    var lastCall, timeoutId;
+    return function () {
+        var now = new Date().getTime();
+        if (lastCall && now < (lastCall + interval) ) {
+            // if we are inside the interval we wait
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(function () {
+                lastCall = now;
+                fn.call();
+            }, interval - (now - lastCall) );
+        } else {
+            // otherwise, we directly call the function 
+            lastCall = now;
+            fn.call();
+        }
+    };
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+function highlightNavigation() {
+    // get the current vertical position of the scroll bar
+    var scrollPosition = $(window).scrollTop();
+
+    // iterate the sections
+    $sections.each(function() {
+        var currentSection = $(this);
+        // get the position of the section
+        var sectionTop = currentSection.offset().top - 1;
+
+        // if the user has scrolled over the top of the section
+
+        if (scrollPosition >= sectionTop) {
+
+            // get the section id
+            var id = currentSection.attr('id');
+            // get the corresponding navigation link
+            var $navigationLink = sectionIdTonavigationLink[id];
+            // if the link is not active
+            if (!$navigationLink.hasClass('active')) {
+
+                // remove .active class from all the links
+                $navigationLinks.removeClass('active');
+                // add .active class to the current link
+                $navigationLink.addClass('active');
+            }
+            // we have found our section, so we return false to exit the each loop
+            return false;
+        }
+    });
 }
 
+$(window).scroll( throttle(highlightNavigation,100) );
+
+// if you don't want to throttle the function use this instead:
+// $(window).scroll( highlightNavigation );
